3dc2437c1882a4f0ed568b5f524d822619a914e6
Koomin <bartoszkominko@gmail.com>
2023-12-04 18:36:40 +0100

    Create core base model and modules service and documents.

---
+++ crm/core/models.py
@@ -0,0 +1,12 @@
+import uuid as uuid_lib
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
