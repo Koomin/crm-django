ee9bc9653114ffd5b9ffc547292cd70f256c46f0
Koomin <bartoszkominko@gmail.com>
2023-12-19 18:54:03 +0100

    Add DocumentType serializer.

--- crm/documents/optima_api/serializers.py
+++ crm/documents/optima_api/serializers.py
@@ -6,4 +6,11 @@ class DocumentTypeSerializer(BaseOptimaSerializer):
     model = DocumentType
 
     def _deserialize(self) -> dict:
-        return {}
+        return {
+            "optima_id": self.obj[0],
+            "obj_class": self.obj[1],
+            "symbol": self.obj[2],
+            "name": self.obj[3],
+            "numbering_scheme": self.obj[4],
+            "active": self.obj[5],
+        }
