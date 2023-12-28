a40f87c094c4970d0f5c36b2e36b7b05113c1cf4
Koomin <bartoszkominko@gmail.com>
2023-12-28 09:13:40 +0100

    Fix import warehouses task.

--- crm/warehouses/tasks.py
+++ crm/warehouses/tasks.py
@@ -3,11 +3,11 @@ from crm.warehouses.models import Warehouse
 from crm.warehouses.optima_api.serializers import WarehouseSerializer
 from crm.warehouses.optima_api.views import WarehouseObject
 
+
 @celery_app.task()
 def import_warehouses():
     warehouse_object = WarehouseObject()
     warehouses = warehouse_object.get()
     for obj in warehouses:
         serializer = WarehouseSerializer(obj)
-        Warehouse.objects.create(**serializer)
-
+        Warehouse.objects.create(**serializer.data)
