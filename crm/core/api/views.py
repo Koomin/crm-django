be95c56a507bddeb7f17f97a572364e06fde87d6
Koomin <bartoszkominko@gmail.com>
2023-12-29 10:41:26 +0100

    Add pagination class.

--- crm/core/api/views.py
+++ crm/core/api/views.py
@@ -1,5 +1,12 @@
 import django_filters
 from rest_framework import permissions, viewsets
+from rest_framework.pagination import PageNumberPagination
+
+
+class BasePagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = "page_size"
+    max_page_size = 1000
 
 
 class BaseViewSet(viewsets.GenericViewSet):
@@ -7,3 +14,4 @@ class BaseViewSet(viewsets.GenericViewSet):
     lookup_field = "uuid"
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_fields = ["uuid"]
+    pagination_class = BasePagination
