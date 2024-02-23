c40bcdd15e0800b07e8f3ea3966dec20fa8e01d0
Koomin <bartoszkominko@gmail.com>
2024-02-23 11:42:39 +0100

    Fix.

--- crm/service/optima_api/views.py
+++ crm/service/optima_api/views.py
@@ -37,7 +37,7 @@ class AttributeObject(OptimaObject):
     )
 
     def get(self, order_id):
-        self.get_queryset.format(order_id)
+        self.get_queryset = self.get_queryset.format(order_id)
         super().get()
 
 
