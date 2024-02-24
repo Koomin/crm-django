033882b89e83011588553deac470969887aac6ea
Koomin <bartoszkominko@gmail.com>
2024-02-24 14:22:22 +0100

    Add routes to states and countries.

--- crm/crm_config/api/views.py
+++ crm/crm_config/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from crm.core.api.views import BaseViewSet
 from crm.crm_config.api.serializers import CountrySerializer, StateSerializer
@@ -8,8 +9,10 @@ from crm.crm_config.models import Country, State
 class CountryViewSet(ListModelMixin, BaseViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class StateViewSet(ListModelMixin, BaseViewSet):
     queryset = State.objects.all()
     serializer_class = StateSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
