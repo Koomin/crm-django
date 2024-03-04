55cffe9ffa076eeb08ebcb6053cdfae09498b6b1
Koomin <bartoszkominko@gmail.com>
2024-03-04 09:45:39 +0100

    Check if serializer returning data.

--- crm/service/tasks.py
+++ crm/service/tasks.py
@@ -145,6 +145,7 @@ def import_service_parts():
         objects = service_part_object.get(order.optima_id)
         if objects:
             for obj in objects:
+                print(obj)
                 serializer = ServicePartSerializer(obj)
                 if serializer.data:
                     try:
