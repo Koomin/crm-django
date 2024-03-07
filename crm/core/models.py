f422a2b7fcee95350425acd4db05287ddf8630bb
Koomin <bartoszkominko@gmail.com>
2024-03-07 20:25:25 +0100

    Add general settings to store basic settings.

--- crm/core/models.py
+++ crm/core/models.py
@@ -1,5 +1,6 @@
 import uuid as uuid_lib
 
+from django.apps import apps
 from django.db import models
 
 
@@ -17,6 +18,8 @@ class OptimaModel(BaseModel):
     exported = models.BooleanField(default=False)
 
     class Meta:
+        optima_update = False
+        optima_create = False
         abstract = True
 
     def __str__(self):
@@ -24,3 +27,25 @@ class OptimaModel(BaseModel):
             return self.code
         except AttributeError:
             return super().__str__()
+
+    def _export_to_optima(self):
+        pass
+
+    def _update_optima_obj(self):
+        pass
+
+    def _optima_synchronization(self):
+        general_settings_model = apps.get_model("crm.crm_config", "GeneralSettings")
+        try:
+            general_settings = general_settings_model.objects.first()
+        except general_settings_model.DoesNotExist:
+            general_settings = None
+        if general_settings and general_settings.optima_synchronization:
+            if self.exported and self.optima_id and self.__class__._meta.optima_update:
+                self._update_optima_obj()
+            elif not self.exported and not self.optima_id and self.__class__._meta.optima_create:
+                self._export_to_optima()
+
+    def save(self, *args, **kwargs):
+        self._optima_synchronization()
+        super().save()
--- crm/crm_config/models.py
+++ crm/crm_config/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from crm.core.models import BaseModel, OptimaModel
@@ -19,3 +20,14 @@ class EmailTemplate(BaseModel):
     name = models.CharField(max_length=255)
     template = models.TextField()
     subject = models.CharField(max_length=255)
+
+
+class GeneralSettings(BaseModel):
+    optima_synchronization = models.BooleanField(default=False)
+    mailing = models.BooleanField(default=False)
+    optima_config_database = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        if not self.pk and GeneralSettings.objects.exists():
+            return ValidationError("Only one settings can be saved")
+        return super().save(*args, **kwargs)
